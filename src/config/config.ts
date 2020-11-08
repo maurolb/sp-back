16adca9efe9a87c6a651de5830b9870260c72ce7
maurolb <maurobritez96@gmail.com>
2020-11-08 13:37:05 -0300

    fix

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,6 @@
 export default {
     DB: {
-        URI: 'mongodb://uzijxvqdtwrcwe4tbcx4:zHOLZzmuKPFnx3riVnsP@b3hv3ljfarnesmj-mongodb.services.clever-cloud.com:27017/b3hv3ljfarnesmj',
+        URI: 'b3hv3ljfarnesmj-mongodb.services.clever-cloud.com',
         USER: 'uzijxvqdtwrcwe4tbcx4',
         PASSWORD: 'zHOLZzmuKPFnx3riVnsP',
         DBNAME: 'b3hv3ljfarnesmj'
